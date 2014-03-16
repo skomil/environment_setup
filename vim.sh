755a0233d2d4e7690638943db16ba392803f75db
skomil <miloogle@gmail.com>
2014-03-16 02:26:27 +0000

    add janus dir and colorscheme

--- vim.sh
+++ vim.sh
@@ -2,4 +2,6 @@
 cd ~
 curl -Lo- https://bit.ly/janus-bootstrap | bash
 rm .vimrc.after
-ln -s environment_setup/vimrc.after ~/.vimrc.after
+ln -s environment_setup/vim/vimrc.after ~/.vimrc.after
+rm .janus
+ln -s environment_setup/vim/janus ~/.janus
