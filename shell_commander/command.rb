14e8fc9ac3ec3dadb4bd2bd7f26421fb961479f8
skomil <miloogle@gmail.com>
2014-03-16 15:47:59 +0000

    add installation steps

--- shell_commander/command.rb
+++ shell_commander/command.rb
@@ -1,5 +1,11 @@
 module ShellCommander
   class Command
+    PRE_INSTALL  = 'PRE_INSTALL'
+    INSTALL = 'INSTALL'
+    POST_INSTALL = 'POST_INSTALL'
+    def initialize(exec_string,action=INSTALL)
+      
+    end
 
   end
 end
