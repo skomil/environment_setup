e44374028f30c20e8b8bb89a31cf944ecf30fff7
Scott Miller <evan.miel@gmail.com>
2014-04-18 21:38:50 -0400

    update command line

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ if [ -z "$DEV_ENV_INIT" ]
 then
   echo 'adding setup to .bashrc....'
   echo  'source ~/environment_setup/script/bash_extra.sh' >> ~/.bashrc
+  if [ "$(eval uname -a | grep Darwin)" ]
+  then
+    echo  'source ~/environment_setup/script/bash_extra.sh' >> ~/.bash_profile
+  fi
   source ~/environment_setup/script/bash_extra.sh
 else
   echo 'Development Environment is already installed.'
--- script/bash_extra.sh
+++ script/bash_extra.sh
@@ -1,5 +1,8 @@
 export PATH=$PATH:~/environment_setup/script
+export ENV_SETUP=~/environment_setup
 export DEV_ENV_INIT=true
+#Set command prompt
+PS1="\e[1;2m\]\u\e[m\][\[\e[1;34m\]\W\[\e[m\]]\e[1;36m\](DFT)\e[m\]\e[1;40m\]\h\e[m\]: "
 echo '*****************************************************'
 echo '*        Development Environment Initialized        *'
 echo '*****************************************************'
