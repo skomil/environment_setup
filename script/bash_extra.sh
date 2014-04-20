7c0ddb6cab5af914dca01404abec3732bf699dae
Scott Miller <evan.miel@gmail.com>
2014-04-20 12:40:24 -0400

    add 256 color mode

--- script/bash_extra.sh
+++ script/bash_extra.sh
@@ -1,6 +1,7 @@
 export PATH=$PATH:~/environment_setup/script
 export ENV_SETUP=~/environment_setup
 export DEV_ENV_INIT=true
+export TERM=xterm-256color
 #Set command prompt
 PS1="\e[1;2m\]\u\e[m\][\[\e[1;34m\]\W\[\e[m\]]\e[1;36m\](DFT)\e[m\]\e[1;40m\]\h\e[m\]: "
 echo '*****************************************************'
