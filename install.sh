2072edc0c4098ce15a207d274c473b0d65f1055f
Scott Miller <evan.miel@gmail.com>
2014-05-18 12:20:06 -0400

    add homebrew support

--- install.sh
+++ install.sh
@@ -12,5 +12,14 @@ then
 else
   echo 'Development Environment is already installed.'
 fi
-install_ruby.sh
+~/environment_setup/script/install_ruby.sh
+
+#If on a mac, install homebrew
+if [ "$(eval uname -a | grep Darwin)" ]
+then
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+fi
+
+#run shell commander installation
 rake install
+
--- script/install_ruby.sh
+++ script/install_ruby.sh
@@ -5,3 +5,4 @@ rvm install ruby-2.0.0-p247
 rvm gemset create rails4
 rvm gemset use rails4
 gem install rails
+
